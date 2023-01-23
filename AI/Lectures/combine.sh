8aaea95a9f6bc042b073aa096bbd94539da70cb7
David Lewis <davidalewis00@gmail.com>
2023-01-23 13:04:39 -0500

    added ai review notes

---
+++ AI/Lectures/combine.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+pdfcombine output.pdf chapter01.pdf chapter02.pdf chapter03.pdf chapter04a.pdf chapter04b.pdf chapter05-6pp.pdf chapter06.pdf chapter07.pdf chapter08.pdf chapter09.pdf chapter13.pdf chapter16.pdf chapter18.pdf chapter20b.pdf
