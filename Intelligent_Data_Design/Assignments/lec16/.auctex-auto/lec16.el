bac2cb398f9c9b878901b6e9f9d674f5fdf42382
David Lewis <davidalewis00@gmail.com>
2023-03-04 10:44:34 -0500

    stuff

---
+++ Intelligent_Data_Design/Assignments/lec16/.auctex-auto/lec16.el
@@ -0,0 +1,46 @@
+(TeX-add-style-hook
+ "lec16"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("geometry" "margin=0.5in") ("algorithm2e" "ruled") ("ulem" "normalem")))
+   (add-to-list 'LaTeX-verbatim-environments-local "minted")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "geometry"
+    "syntax"
+    "pdfpages"
+    "tcolorbox"
+    "etoolbox"
+    "environ"
+    "algorithm2e"
+    "graphicx"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "amssymb"
+    "capt-of"
+    "hyperref"
+    "minted"
+    "physics")
+   (LaTeX-add-labels
+    "sec:org23908c0"
+    "sec:org7d2f4a0"
+    "sec:orgd037ffd"
+    "sec:orgfc286dc"
+    "sec:org3e4b307"))
+ :latex)
+
